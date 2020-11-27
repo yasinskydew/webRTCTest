f8fe98ef8f5a0a0e20981ec03b966f5a0f433cf6
vladimir <uladzimir.yasinski@webfriends.by>
2020-11-27 14:46:21 +0300

    Fix port

--- app.js
+++ app.js
@@ -10,27 +10,27 @@ const server = http.createServer(app);
 
 const io = socket(server);
 app.use(express.static(__dirname + "/public"));
-
-io.sockets.on("error", e => console.log(e));
-io.sockets.on("connection", socket => {
-  socket.on("broadcaster", () => {
-    broadcaster = socket.id;
-    socket.broadcast.emit("broadcaster");
-  });
-  socket.on("watcher", () => {
-    socket.to(broadcaster).emit("watcher", socket.id);
-  });
-  socket.on("offer", (id, message) => {
-    socket.to(id).emit("offer", socket.id, message);
-  });
-  socket.on("answer", (id, message) => {
-    socket.to(id).emit("answer", socket.id, message);
-  });
-  socket.on("candidate", (id, message) => {
-    socket.to(id).emit("candidate", socket.id, message);
-  });
-  socket.on("disconnect", () => {
-    socket.to(broadcaster).emit("disconnectPeer", socket.id);
-  });
-});
+//
+// io.sockets.on("error", e => console.log(e));
+// io.sockets.on("connection", socket => {
+//   socket.on("broadcaster", () => {
+//     broadcaster = socket.id;
+//     socket.broadcast.emit("broadcaster");
+//   });
+//   socket.on("watcher", () => {
+//     socket.to(broadcaster).emit("watcher", socket.id);
+//   });
+//   socket.on("offer", (id, message) => {
+//     socket.to(id).emit("offer", socket.id, message);
+//   });
+//   socket.on("answer", (id, message) => {
+//     socket.to(id).emit("answer", socket.id, message);
+//   });
+//   socket.on("candidate", (id, message) => {
+//     socket.to(id).emit("candidate", socket.id, message);
+//   });
+//   socket.on("disconnect", () => {
+//     socket.to(broadcaster).emit("disconnectPeer", socket.id);
+//   });
+// });
 server.listen(port, () => console.log(`Server is running on ${port}`));

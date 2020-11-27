f052832a62a27204bacd77e0c1ce74431f5aa332
vladimir <uladzimir.yasinski@webfriends.by>
2020-11-27 14:26:03 +0300

    Add web cam and voice

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 let broadcaster;
 const port = 4000;
 
-const http = require("http");
+const http = require("https");
 const server = http.createServer(app);
 
 const io = socket(server);

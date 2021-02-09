05a54dac7f82062f0bfeed752f889ffd24e5d269
vladimir <uladzimir.yasinski@webfriends.by>
2021-02-09 09:52:20 +0300

    config

--- app.js
+++ app.js
@@ -15,38 +15,38 @@ app.use(express.static(__dirname + "/public"));
 io.sockets.on("error", e => console.log(e));
 io.sockets.on("connection", socket => {
   RTCMultiConnectionServer.addSocket(socket, {
-    "socketURL": "https://webrtc.musio.io",
+    "socketURL": "/",
     "dirPath": "",
     "socketMessageEvent": "RTCMultiConnection-Message",
     "socketCustomEvent": "RTCMultiConnection-Custom-Message",
-    "port": "9001",
+    "port": "3000",
     "enableLogs": "false",
     "autoRebootServerOnFailure": "false",
     "enableAdmin": "false",
     "adminUserName": "username",
     "adminPassword": "password"
   });
-  socket.on("broadcaster", () => {
-    console.log("broadcaster")
-    broadcaster = socket.id;
-    socket.broadcast.emit("broadcaster");
-  });
-  socket.on("watcher", () => {
-    console.log("watcher")
-    socket.to(broadcaster).emit("watcher", socket.id);
-  });
-  socket.on("offer", (id, message) => {
-    console.log("offer")
-    socket.to(id).emit("offer", socket.id, message);
-  });
-  socket.on("answer", (id, message) => {
-    console.log("answer")
-    socket.to(id).emit("answer", socket.id, message);
-  });
-  socket.on("candidate", (id, message) => {
-    console.log("candidate")
-    socket.to(id).emit("candidate", socket.id, message);
-  });
+  // socket.on("broadcaster", () => {
+  //   console.log("broadcaster")
+  //   broadcaster = socket.id;
+  //   socket.broadcast.emit("broadcaster");
+  // });
+  // socket.on("watcher", () => {
+  //   console.log("watcher")
+  //   socket.to(broadcaster).emit("watcher", socket.id);
+  // });
+  // socket.on("offer", (id, message) => {
+  //   console.log("offer")
+  //   socket.to(id).emit("offer", socket.id, message);
+  // });
+  // socket.on("answer", (id, message) => {
+  //   console.log("answer")
+  //   socket.to(id).emit("answer", socket.id, message);
+  // });
+  // socket.on("candidate", (id, message) => {
+  //   console.log("candidate")
+  //   socket.to(id).emit("candidate", socket.id, message);
+  // });
   // socket.on("disconnect", () => {
   //   console.log("disconnect")
   //   socket.to(broadcaster).emit("disconnectPeer", socket.id);

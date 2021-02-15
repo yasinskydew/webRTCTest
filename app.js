425db3c7282518042e617be99deada3c166bcf6a
vladimir <uladzimir.yasinski@webfriends.by>
2021-02-15 13:38:11 +0300

    Add ssl v3

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const socket = require("socket.io");
 const RTCMultiConnectionServer = require('rtcmulticonnection-server');
-const app = express();
 const cors = require('cors');
 const path = require('path');
-const port = process.env.PORT || 3000;
-
 const http = require("http");
-const server = http.createServer(app);
-
-const io = socket(server);
+const app = express();
 app.use(cors());
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socket(server);
 
 io.sockets.on("error", e => console.log(e));
 io.sockets.on("connection", socket => {
@@ -20,14 +18,12 @@ io.sockets.on("connection", socket => {
     socketMessageEvent: 'RTCMultiConnection-Message',
     socketCustomEvent: 'RTCMultiConnection-Custom-Message',
     port: '9001',
-    enableLogs: 'false',
-    isUseHTTPs: 'true',
+    enableLogs: false,
+    isUseHTTPs: true,
     sslKey: path.join(__dirname, 'ssl', 'private.key'),
     sslCert: path.join(__dirname, 'ssl', 'certificate.crt'),
     autoRebootServerOnFailure: false,
-    enableAdmin: false,
-    adminUserName: 'username',
-    adminPassword: 'password',
+    enableAdmin: false
   });
 });
 server.listen(port, () => console.log(`Server is running on ${port}`));
